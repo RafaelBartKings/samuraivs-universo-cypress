76c676f228e4c24822c4ae00b646cce7bcf58952
Rafael  Reis <rafyangtj@gmail.com>
2022-03-22 22:53:16 -0300

    config-datebase-cypress.json

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,13 +22,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'jelani.db.elephantsql.com',
-    user: 'uyxaafbz',
-    password: 'yoPKzb4xAr2zGy4LLLKvXF_5P2KYNmJj',
-    database: 'uyxaafbz',
-    porta: 5432
-  })
+  const configJson = require(config.configFile)
+
+  const pool = new Pool(configJson.dbConfig)
 
   on('task', {
     removeUser(email){
